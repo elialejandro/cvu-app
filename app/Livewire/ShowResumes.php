f9187fe9b32adcaf9f73f3f745d4838032233398
Eli Alejandro <iscelialejandro@gmail.com>
2023-11-20 01:18:24 -0600

    feat(components): update show resumes with edit link

--- app/Livewire/ShowResumes.php
+++ app/Livewire/ShowResumes.php
@@ -3,22 +3,29 @@
 namespace App\Livewire;
 
 use App\Models\Resume;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
 use Livewire\Component;
 
 class ShowResumes extends Component
 {
-    public function render()
+    public function render(): View
     {
-        return view('livewire.show-resumes');
+        $resumes = Resume::query()->where('user_id', auth()->id())->get();
+        return view('livewire.show-resumes', [
+            'resumes' => $resumes
+        ]);
     }
 
     public function create()
     {
         $resume = Resume::create([
+            'user_id' => auth()->user()->id,
             'title' => 'Nuevo CV'
         ]);
 
-        return redirect()->route('resume.edit', ['id' => $resume->id]);
+        return redirect()->route('resume.edit', ['resume' => $resume->id]);
     }
 
 }
--- resources/views/livewire/show-resumes.blade.php
+++ resources/views/livewire/show-resumes.blade.php
@@ -5,15 +5,19 @@
     </div>
 
     <div class="grid grid-cols-3 gap-4">
+        @foreach($resumes as $resume)
         <div class="p-4 bordered shadow-md rounded-md">
-            <div class="text font-bold">Resume 1</div>
+            <div class="text font-bold">{{ $resume->title }}</div>
             <div class="py-2">
-                19 de noviembre de 2023
+                <span class="text-gray-500">{{ $resume->created_at->locale('es-MX.UTF8')->isoFormat('DD [de] MMMM [de] YYYY, HH:mm a')  }}</span>
             </div>
             <div class="flex gap-3 justify-end">
-                <x-button>Editar</x-button>
+                <a href="{{ route('resume.edit', ['resume' => $resume->id]) }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
+                    Editar
+                </a>
                 <x-danger-button>Borrar</x-danger-button>
             </div>
         </div>
+        @endforeach
     </div>
 </div>
